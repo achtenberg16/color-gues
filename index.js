c4b89e07568fe0b8bb1819789b9ad347dde1ce0a
achtenberg16 <achtenberg16@gmail.com>
2022-05-12 20:04:40 -0300

    feat: implementado funções de gerar cor

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function main () {
-  
+function generateNumber() {
+  return Math.floor(Math.random() * 255);
 }
+
+function generateColor() {
+  return `rgb(${generateNumber()}, ${generateNumber()}, ${generateNumber()})`;
+}
+
+function main () {
+
+}
+
+window.onload = main;
