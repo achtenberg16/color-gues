3b25b2dfb4201a60fa81f66695eadaadb9270ecc
achtenberg16 <achtenberg16@gmail.com>
2022-05-12 21:53:06 -0300

    feat: adicionado funcionalidade de criar elementos e inserir elementos no html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const containerOptions = document.querySelector('.options-container');
+
 function generateNumber() {
   return Math.floor(Math.random() * 255);
 }
@@ -6,8 +8,28 @@ function generateColor() {
   return `rgb(${generateNumber()}, ${generateNumber()}, ${generateNumber()})`;
 }
 
-function main() {
+function generateElement(tag, className) {
+  const element = document.createElement(tag);
+  if (className) {
+    element.classList.add(className);
+  }
+  return element;
+}
 
+function appendElement(element, parent) {
+  parent.appendChild(element);
+}
+
+function createOptions(quantity) {
+  for (let i = 0; i < quantity; i += 1) {
+    const element = generateElement('div', 'option');
+    appendElement(element, containerOptions);
+  }
+}
+
+function main() {
+  const TOTAL_OPTIONS = 5;
+  createOptions(TOTAL_OPTIONS);
 }
 
 window.onload = main;
