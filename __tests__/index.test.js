58247b912a589df6cc649021401ce8178efc0884
achtenberg16 <achtenberg16@gmail.com>
2022-05-12 21:37:02 -0300

    test: adicionado novo teste

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,9 @@
-const { generateColor, generateNumber } = require('../index');
+const { test } = require('@jest/globals');
+const { generateColor } = require('../index');
 
 describe('testa a função generate color', () => {
   test('deve retornar uma string com a cor 255, 255, 255', () => {
-    generateNumber.mockReturnValue(255);
+    jest.spyOn(global.Math, 'random').mockReturnValue(1);
     expect(generateColor()).toBe('rgb(255, 255, 255)');
   });
 });
